a3ba66d14370a75225e1b9f42504c7088afee5e5
CHANDANA <palakurlachandana85@gmail.com>
2025-05-23 16:47:48 +0530

    Add files via upload

---
+++ button.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("mybutton");
+const messagediv = document.getElementById("message");
+
+button.addEventListener('click', () => {
+    messagediv.textContent = 'LIVELOVELAUGH';
+});
